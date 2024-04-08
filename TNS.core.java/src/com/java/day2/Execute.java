1754dbbb8eeb11610429d4f094049e32d6f6366b
Admin <Admin@DESKTOP-00283AI>
2024-04-08 15:28:23 +0530

    Day 3

---
+++ TNS.core.java/src/com/java/day2/Execute.java
@@ -0,0 +1,15 @@
+package com.java.day2;
+
+//Demo on "Has a " Relationship 
+public class Execute {
+	
+	public static void main(String args[]) {
+	Address A1 = new Address("Dadar","Dadar Station","Mumbai");
+	
+	Person p1 = new Person("Akshad","Mahadik",A1);
+	
+	System.out.println(p1);
+	
+	}
+	
+}
---
+++ TNS.core.java/src/com/java/day2/Person.java
@@ -0,0 +1,39 @@
+package com.java.day2;
+
+public class Person {
+	private String FirstName;
+	private String LastName;
+	private Address Address;
+	
+
+	public Person(String firstName, String lastName, Address address) {
+		super();
+		FirstName = firstName;
+		LastName = lastName;
+		Address = address;
+	}
+	public String getFirstName() {
+		return FirstName;
+	}
+	public void setFirstName(String firstName) {
+		FirstName = firstName;
+	}
+	public String getLastName() {
+		return LastName;
+	}
+	public void setLastName(String lastName) {
+		LastName = lastName;
+	}
+	public com.java.day2.Address getAddress() {
+		return Address;
+	}
+	public void setAddress(Address address) {
+		Address = address;
+	}
+
+	@Override
+	public String toString() {
+		return "Person"+"\n"+"FirstName="+ FirstName + "\n"+"LastName=" + LastName +"\n"+ "Address=" + Address + "]";
+	}
+	
+}
