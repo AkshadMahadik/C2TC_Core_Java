5271958e5127485524e2a5480dc29ac449f84bee
AkshadMahadik <122466863+AkshadMahadik@users.noreply.github.com>
2024-06-18 11:55:39 +0530

    Initial Commit.

---
+++ TNS.core.java/src/com/java/LinkList/ArrayListCheck.java
@@ -0,0 +1,17 @@
+package com.java.LinkList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListCheck {
+
+    public static void main(String[] args) {
+        List<String> al = new ArrayList<>();
+        System.out.print(al.size());
+        al.add("Akashd");
+        al.add("vu1f2122001");
+        al.add("Dadar");
+        al.add("Mumbai");
+        System.out.println(al);
+    }
+}
